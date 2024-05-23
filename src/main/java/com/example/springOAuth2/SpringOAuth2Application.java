527bf88c825c189fb6c3ed38e8d678b79f5d1adf
sushma pagadala <88230359+sushma2026@users.noreply.github.com>
2024-05-23 11:04:19 +0530

    google Authentication added and ....
    error message issues doing on

---
+++ src/main/java/com/example/springOAuth2/SpringOAuth2Application.java
@@ -0,0 +1,64 @@
+package com.example.springOAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class SpringOAuth2Application extends WebSecurityConfigurerAdapter {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    @GetMapping("/error")
+    public String error(HttpServletRequest request){
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOAuth2Application.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+
+        http.antMatcher("/**")
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**","/logout").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
+                .oauth2Login(o->o.failureHandler((request, respond, exception)->{
+                    request.getSession().setAttribute("error message", exception.getMessage());
+                    handler.onAuthenticationFailure(request,respond,exception);
+                }));
+    }
+}
